f99e7fe58e0a96a24c08e4d0ed704c807278efdb
IPO <ipo@ipo.org>
2017-11-11 12:39:12 +0100

    registro e identificacion correctos

--- app/src/main/java/org/example/viajes/ConsultaBD.java
+++ app/src/main/java/org/example/viajes/ConsultaBD.java
@@ -20,7 +20,55 @@ public class ConsultaBD {
         bdw = BaseDeDatos.getWritableDatabase();
     }
 
-    //Cursor con la lista de itinerarios de un cliente, donde id es el identiticador unico del cliente
+
+    //Registro e Identificacion
+
+    //Comprobar que el e-mail no existe
+    public static boolean emailUnico(String email){
+        boolean unico = false;
+        SQLiteDatabase bdw = BaseDeDatos.getReadableDatabase();
+        Cursor c = bdw.rawQuery("SELECT * FROM user WHERE email = '"+email+"'", null);
+        if (c.getCount()==0){
+            unico = true;
+        }
+        c.close();
+        return unico;
+    }
+
+    //Registrar un nuevo usuario
+    public static boolean newUser(String email, String password, String name, String surname){
+        boolean correcto = true;
+        try{
+            bdw.execSQL("INSERT INTO user (email,password,name,surname) VALUES ('"+email+"' , '"+password+"' , '"+name+"' , '"+surname+"')");
+        }
+        catch (Exception e){
+            correcto = false;
+        }
+
+        return correcto;
+    }
+
+    //Identificarse (devuelve -1 si el usuario no esta registrado, -2 en caso de error en lectura de la base de datos, si va bien devuelve el identificador del usuario)
+    public static int identificar(String email, String password){
+        int id = -1;
+        try {
+            Cursor c = bdw.rawQuery("SELECT * FROM user WHERE email = '"+email+"' AND password = '"+password+"'",null);
+            if(c.moveToNext()){
+                id=c.getInt(0);
+            }
+            c.close();
+        }catch (Exception e){
+            id = -2;
+        }
+
+        return id;
+    }
+
+
+
+
+
+    //Cursor con la lista de itinerarios de un cliente, donde id es el identiticador unico del usuario
     public static Cursor listadoItinerarios(int id){
         SQLiteDatabase bdw = BaseDeDatos.getReadableDatabase();
         return bdw.rawQuery("SELECT route_id AS _id, * FROM route " +
--- app/src/main/java/org/example/viajes/MainActivity.java
+++ app/src/main/java/org/example/viajes/MainActivity.java
@@ -6,17 +6,24 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements  AdaptadorItinerarios.OnItemClickListener{
 
     public AdaptadorItinerarios adaptador;
     public RecyclerView recyclerViewClientes;
+    public EditText email, pass, nombre,surname;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //setContentView(R.layout.pruebaregistro);
+
+
+
+
 
         //referencio el reciclerview
         recyclerViewClientes = (RecyclerView) findViewById(R.id.prueba);
@@ -24,6 +31,14 @@ public class MainActivity extends AppCompatActivity implements  AdaptadorItinera
 
         //creo el recicler view
         listaitinerarios();
+
+        /* //refererencio los campos de pruebaregistro
+
+        email = findViewById(R.id.correo);
+        pass = findViewById(R.id.contraseña);
+        nombre = findViewById(R.id.usuario);
+        surname = findViewById(R.id.surname);
+*/
 
     }
 
@@ -57,5 +72,44 @@ public class MainActivity extends AppCompatActivity implements  AdaptadorItinera
     }
 
     //en el caso de crear o borrar un itinerario sera aconsejable volver a llamar la funcion listaitinerario() para que actualice la vista
+/*
+    public void registrar(View v){
+        //inicializo la base de datos, si no existe la crea
+        ConsultaBD.inicializaBD(this);
+        if(ConsultaBD.newUser(email.getText().toString(),pass.getText().toString(),nombre.getText().toString(),surname.getText().toString())){
+            Toast.makeText(MainActivity.this, "Registrado correctamente", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(MainActivity.this, "Error en el pruebaregistro", Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+    public void unico(View v){
+        //inicializo la base de datos, si no existe la crea
+        ConsultaBD.inicializaBD(this);
+
+        String correo = email.getText().toString();
+        if(ConsultaBD.emailUnico(correo)){
+            Toast.makeText(MainActivity.this, "El correo no existe", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(MainActivity.this, "el correo ya existe", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void identificar(View v){
+        //inicializo la base de datos, si no existe la crea
+        ConsultaBD.inicializaBD(this);
+        int user_id = ConsultaBD.identificar(email.getText().toString(), pass.getText().toString());
+        if (user_id == -1){
+            Toast.makeText(MainActivity.this, "usuario no registrado", Toast.LENGTH_SHORT).show();
+        }else{if(user_id == -2){
+            Toast.makeText(MainActivity.this, "error en la base de datos", Toast.LENGTH_SHORT).show();
+        }
+            Toast.makeText(MainActivity.this, "usuario "+user_id, Toast.LENGTH_SHORT).show();
+        }
+
+    }
+    */
+
 
 }
